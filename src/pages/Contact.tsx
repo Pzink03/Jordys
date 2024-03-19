5f42bb44fba441725611011671d55b0cd681a8e8
Pete <pete.zink03@gmail.com>
2024-03-18 20:30:02 -0400

    Added Modal confirmation when filling out contact form

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,10 +1,11 @@
 import emailjs from '@emailjs/browser';
-import React from 'react';
+import React, { useState } from 'react';
 import { ICON_LINKS } from "@/components/constants/iconLinks";
 import { Reveal } from '@/components/Reveal';
 
 
 export function Contact(){
+  const [showSuccessMessage, setShowSuccessMessage] = useState(false)
     const sendEmail = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
 
@@ -12,10 +13,10 @@ export function Contact(){
           .sendForm('service_sjd1ze7', 'template_uxfakel', e.currentTarget, {
             publicKey: 'VxcZMHQK7imR1W3Ez',
           })
-          .then(
-            () => {
-              console.log('SUCCESS!');
-            },
+          .then((result) =>{
+            console.log(result.text);
+            setShowSuccessMessage(true)
+          },
             (error) => {
               console.log('FAILED...', error.text);
             },
@@ -40,9 +41,9 @@ export function Contact(){
             When you call us for a free estimate, we will meet with you to discuss your individual needs and review the details of each room. On every visit, we'll provide dusting, vacuuming, cleaning, and sanitizing services for each room from top to bottom. Our typical service includes everything on this list and mor
             </p>
             <form onSubmit={sendEmail} className='flex flex-col max-w-lg w-5/6 md:w-1/2 mt-4'>
-              <input type='text' name='user_name' placeholder='Enter your name' className='p-2 rounded mt-4 bg-transparent border-gray-300 border-2 text-subtext focus:outline-none hover:border-secondary-pink focus:border-secondary-pink transition duration-300' />
-              <input type='text' name='user_email' placeholder='Enter your email' className='rounded my-4 p-2 bg-transparent border-gray-300 border-2  text-white focus:outline-none  hover:border-secondary-pink focus:border-secondary-pink transition duration-300' />
-              <textarea name='message' placeholder='Enter your message' rows={10} className='p-2 bg-transparent border-2 border-gray-300 rounded text-white focus:outline-none hover:border-secondary-pink focus:border-secondary-pink transition duration-300'></textarea>
+              <input type='text' name='user_name' placeholder='Enter your name' className='p-2 rounded mt-4 border-gray-300 border-2 text-subtext focus:outline-none hover:border-secondary-pink focus:border-secondary-pink transition duration-300' />
+              <input type='text' name='user_email' placeholder='Enter your email' className='rounded my-4 p-2 border-gray-300 border-2  text-subtext focus:outline-none  hover:border-secondary-pink focus:border-secondary-pink transition duration-300' />
+              <textarea name='message' placeholder='Enter your message' rows={10} className='p-2 bg-transparent border-2 border-gray-300 rounded text-subtext focus:outline-none hover:border-secondary-pink focus:border-secondary-pink transition duration-300'></textarea>
               <button className='text-white bg-gradient-to-b from-gray-300 to-primary-green px-6 py-3 my-8 mx-auto flex items-center rounded hover:scale-110 duration-300'>
               Let's Talk
               </button>
@@ -59,6 +60,16 @@ export function Contact(){
             </form>
           </div>
         </Reveal>
+        {showSuccessMessage && (
+                        <div className="fixed inset-0 bg-black bg-opacity-30 flex justify-center items-center">
+                            <div className="bg-secondary-pink p-5 w-80 h-40 rounded-xl flex flex-col items-center justify-between shadow-lg">
+                                <p className="text-primary-green font-bold">Message successfully sent!</p>
+                                <button onClick={() => setShowSuccessMessage(false)} className="mt-4 px-10 py-2 font-semibold bg-primary-green text-white rounded hover:scale-110 transition focus:outline-none">
+                                    Thanks!
+                                </button>
+                            </div>
+                        </div>
+                    )}
     </div>
   )
 }
